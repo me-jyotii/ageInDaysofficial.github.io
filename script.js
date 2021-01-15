e17b94fad9b3b9cd515a16cdf765982d8cb63fca
jyoti <jyotisjr76@gmail.com>
2021-01-15 16:23:12 +0530

    get your age in days

---
+++ script.js
@@ -0,0 +1,15 @@
+//age in days
+function ageInDays()
+{
+    var birthYear = prompt("Hey what is your born year ??");
+    var age = (2021 - birthYear) * 365;
+    var h1 = document.createElement('h1');
+    var textAnswer = document.createTextNode('You are '+age+' days old!');
+    h1.setAttribute('id', 'ageInDays');
+    h1.appendChild(textAnswer);
+    document.getElementById('flex-box-result').appendChild(h1);
+}
+function reset()
+{
+    document.getElementById('ageInDays').remove();
+}
